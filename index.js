b8a3a8ad2e602e092ce6481fb9f93152c486dba2
VikasGurjar <r9427173766@gmail.com>
2025-12-28 09:42:57 +0530

    Make ejs page  and stylecss and setUP env files

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express") ;
+const path = require("path") ;
+require("dotenv").config() ;
+const mongoose = require("mongoose") ;
+
 
 const app = express() ;
 
+app.use(express.static(path.join(path.dirname(__filename) , "public"))) ;
+app.use(express.urlencoded({extended:true})) ;
+
+mongoose.connect(process.env.MONGO_URI , {
+    dbName :"imageUploads"
+}).then(()=> console.log("mongodb connected successfully!!")).catch((err)=> console.log(err)) ;
+
 app.get("/",(req , res)=>{
-    res.send("hello image uploader") ;
+    res.render("index.ejs" , {url : null}) ;
 })
 
-const port = 1000 ;
+const port = process.env.PORT || 8000;
 
 app.listen(port , ()=>{
     console.log(`Server is running on the port : ${port}` ) ;
